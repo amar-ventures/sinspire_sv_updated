f76bf97cd5156b7b027a92a78c4f3f23f099300f
anamika tk <anmike.xyz@gmail.com>
2025-03-29 15:09:20 +0530

    fic 7

--- docs/_app/immutable/entry/start.BHJ7X1pZ.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/lH4zKnbo.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
+// Fix: Change import source
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        adapter: adapter({ fallback: 'index.html' }),
-    },
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '404.html',
+      precompress: false
+    }),
+    paths: {
+      base: process.env.BASE_PATH || '',
+    }
+  },
+  preprocess: vitePreprocess()
 };
 
 export default config;
