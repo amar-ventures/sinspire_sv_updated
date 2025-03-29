2c6211d7a63b8f50b542273bc586fc475a39df2b
anamika tk <anmike.xyz@gmail.com>
2025-03-29 15:13:31 +0530

    fix 9

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
-// Fix: Change import source
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: '404.html',
-      precompress: false
-    }),
-    paths: {
-      base: process.env.BASE_PATH || '',
-    }
-  },
-  preprocess: vitePreprocess()
+    kit: {
+        adapter: adapter({ fallback: 'index.html' }),
+    },
 };
 
 export default config;
